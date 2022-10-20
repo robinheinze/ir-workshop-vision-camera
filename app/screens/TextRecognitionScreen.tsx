d63e123817bf41d316d356c4402638773ea9863b
Robin Heinze <robin.m.heinze@gmail.com>
2022-10-20 11:44:49 -0700

    add text recognition frame processor and output

--- app/screens/TextRecognitionScreen.tsx
+++ app/screens/TextRecognitionScreen.tsx
@@ -3,18 +3,31 @@ import { observer } from "mobx-react-lite"
 import { ActivityIndicator, StyleSheet, View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AppStackParamList } from "../navigators"
-import { Camera, useCameraDevices } from "react-native-vision-camera"
+import { Camera, useCameraDevices, useFrameProcessor } from "react-native-vision-camera"
 import { checkCameraPermission } from "../utils/checkCameraPermissions"
 import { useIsFocused } from "@react-navigation/native"
+import { OCRFrame, scanOCR } from "vision-camera-ocr"
+import { runOnJS } from "react-native-reanimated"
 
 export const TextRecognitionScreen: FC<StackScreenProps<AppStackParamList, "TextRecognition">> =
   observer(function TextRecognitionScreen() {
     const devices = useCameraDevices()
     const device = devices.back
     const isFocused = useIsFocused()
+    const [scannedOCR, setScannedOCR] = React.useState<OCRFrame>()
 
     useEffect(() => {
       checkCameraPermission()
+    }, [])
+
+    useEffect(() => {
+      console.log(scannedOCR)
+    }, [scannedOCR.result.text])
+
+    const frameProcessor = useFrameProcessor((frame) => {
+      "worklet"
+      const scannedOcr = scanOCR(frame)
+      runOnJS(setScannedOCR)(scannedOcr)
     }, [])
 
     if (device == null)
@@ -24,7 +37,15 @@ export const TextRecognitionScreen: FC<StackScreenProps<AppStackParamList, "Text
         </View>
       )
 
-    return <Camera style={StyleSheet.absoluteFill} device={device} isActive={isFocused} />
+    return (
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={isFocused}
+        frameProcessor={frameProcessor}
+        frameProcessorFps={5}
+      />
+    )
   })
 
 const $fullScreenCentered: ViewStyle = {
